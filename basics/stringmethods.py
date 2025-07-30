c31edf2afdb12f61aa9a0e666337aeebf04928a5
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-30 13:17:25 +0530

    Create stringmethods.py

---
+++ basics/stringmethods.py
@@ -0,0 +1,18 @@
+subject = "Python Programming   "
+level = "Beginners Level"
+
+print(subject.upper())  # Convert to uppercase
+print(subject.lower())  # Convert to lowercase
+print(subject.title())  # Convert to title case
+print(subject.find("Pro"))  # Find substring
+print(subject.replace("Programming", "Course"))  # Replace substring
+print(subject.replace("P", "j"))
+print(subject.replace("P", "Jo"))
+print(subject.split())  # Split into a list of words
+print(subject.strip())  # Remove leading and trailing spaces
+print(subject.lstrip())
+print(subject.rstrip())
+print("Pro" in subject)
+print("Pro" not in subject)
+print("pro" in subject)
+print(subject)
