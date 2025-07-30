ae6e79f322b20cff9765d112026a530523bee3a6
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-30 11:37:24 +0530

    Update var2.py

--- var2.py
+++ var2.py
@@ -8,3 +8,6 @@ print(len(course))
 print(course[0])  # First character
 print(course[6])  # Seventh character
 print(course[-2])  # Second to last character
+
+print(course[0:5])  # First five characters
+# print(course[7:])  # From the seventh character to the end
