c1b9d1bc9dec761c73e71a8f79664f90fb7682ad
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-30 15:43:38 +0530

    Update number2.py

---
+++ basics/number2.py
@@ -0,0 +1,8 @@
+import math
+
+print(math.sqrt(16))  # Square root of 16
+print(math.ceil(2.3))  # Round up to nearest integer # Output: 4
+print(math.floor(2.7))  # Round down to nearest integer # Output: 2
+print(math.pi)  # Value of pi # Output: 3.141592653589793
+print(math.e)  # Value of e # Output: 2.718281828459281828459045
+print(math.pow(2, 3))  # 2 raised to the power of
