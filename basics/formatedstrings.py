2a7b460de74a7590369c89a47656464b2f5e4593
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-30 12:59:19 +0530

    Update formatedstrings.py

--- basics/formatedstrings.py
+++ basics/formatedstrings.py
@@ -1,7 +1,17 @@
 first = "Prathibha"
 last = "Sithumini"
 
-full = f"{first} {last}"
-fullname = first + " " + last
+full = f"{first} {last}"  # Formatted string
+fullname = first + " " + last  # Concatenation
 print(full)
 print(fullname)
+
+
+subject = "Python"
+level = "beginners"
+
+course = f"{subject} {level}"  # Formatted string
+print(course)
+
+sub = f"{len(subject)} {2+2}"  # Formatted string with expression
+print(sub)  # Output: 6 4
