156ba52216c62a8e0687ba9c2ccf9921ef4ce4cf
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-30 10:52:11 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -3,3 +3,16 @@ y = 2
 unit_price = 300
 
 print(unit_price)
+
+
+student_name = "Prathibha"
+student_score = 95
+rating = "A"
+gpa = 3.831
+is_passing = True
+
+print("Name :" + student_name)
+print("Score :" + student_score)
+print("Grade :" + rating)
+print("GPA :" + gpa)
+print("Status :" + is_passing)
