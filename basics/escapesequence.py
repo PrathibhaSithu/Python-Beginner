1a4a187ba09ffd8317080212902b6b0c75248b01
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-30 12:19:51 +0530

    Update escapesequence.py

--- basics/escapesequence.py
+++ basics/escapesequence.py
@@ -1,3 +1,22 @@
+# Escape sequences in Python
+# \"
+# \'
+# \\
+# \n  # New line
+# \t  # Tab
+
 course = 'Python \"Learning"'
 course = "Python \"Learning"  # String with escape sequence"
+course1 = "Python \"Learning"
+course2 = "Python \'Learning"
+course3 = "Python \\Learning"
+course4 = "Python \nLearning"
+course5 = "Python \tLearning"
+
+
 print(course)
+print(course1)
+print(course2)
+print(course3)
+print(course4)
+print(course5)
