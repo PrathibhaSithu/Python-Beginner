a35dabbbdf401ca2585ea4c218ac53ea71495f5f
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-30 13:33:02 +0530

    Update numbers.py

---
+++ basics/numbers.py
@@ -0,0 +1,22 @@
+x = 1
+x = 1.1
+x = 1+2j
+
+print(x)
+
+print(type(x))  # Output: <class 'complex'>
+
+print(type(1))  # Output: <class 'int'>
+print(type(1.1))  # Output: <class 'float'>
+print(type(1+2j))  # Output: <class 'complex'>
+
+print(1 + 2)  # Addition
+print(1 - 2)  # Subtraction
+print(1 * 2)  # Multiplication
+print(1 / 2)  # Division
+print(1 // 2)  # Floor division
+print(1 % 2)  # Modulus
+print(2 ** 3)  # Exponentiation
+print(abs(-2))  # Absolute value
+print(round(2.9))  # Round to nearest integer
+print(round(2.5))  # Round to nearest even integer
