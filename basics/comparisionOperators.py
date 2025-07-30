1da565709e3cfcdea9d497b692ed9af5b5348b05
PrathiSithu <sithuminiprathiba@gmail.com>
2025-07-31 00:46:00 +0530

    Create comparisionOperators.py

---
+++ basics/comparisionOperators.py
@@ -0,0 +1,5 @@
+print(ord("b"))
+
+print(ord("B"))  # ASCII value of 'B'  # True, non-empty string is considered True
+print(bool("0"))  # True, non-empty string is considered True
+print(ord("0"))  # ASCII value of '0'  # Output: 48
