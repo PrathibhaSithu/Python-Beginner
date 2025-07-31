e3f2e6cceb8bd39108ae2fd197d53f204f9eed45
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-31 12:46:37 +0530

    Update forelse.py

--- fundamentals/forelse.py
+++ fundamentals/forelse.py
@@ -1,6 +1,8 @@
-successful = True
+successful = False
 for number in range(4):
     print("Line", number + 1, (number + 1) * "*")
     if successful:
-        print("Success on line")
+        print("Success on line no: " + str(number))
         break
+    else:
+        print("No success found in the loop")
