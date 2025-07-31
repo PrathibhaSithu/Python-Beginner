09dd6bf55f4967a8f01bba4981e23257f66e894f
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-31 11:00:53 +0530

    Update forloop.py

--- fundamentals/forloop.py
+++ fundamentals/forloop.py
@@ -1,2 +1,11 @@
 for number in range(4):
-    print("*")
+    print("Line", number + 1, (number + 1) * "*")
+
+for pattern1 in range(4):
+    print((pattern1 + 1) * "!")
+
+for pattern2 in range(4, 1):
+    print((pattern2 + 1) * "****")
+
+for pattern2 in range(1, 10, 2):
+    print("Line", number + 1, (pattern2 + 1) * "****")
