f5978c70f8f2d727ea77200ff0da55c1af4e5bbd
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-31 10:43:21 +0530

    Update ChainComparisonOperators.py

--- fundamentals/ChainComparisonOperators.py
+++ fundamentals/ChainComparisonOperators.py
@@ -4,6 +4,6 @@ age = int(input("Enter your age: "))
 
 # if age >= 18 and age <= 55:
 if 18 <= age < 65:
-    print("You are eligible to be a member of the gym.")
+    print("You are eligible to be a member of the gym. 😊")
 else:
-    print("You are not eligible to be a member of the gym.")
+    print("You are not eligible to be a member of the gym.😒")
