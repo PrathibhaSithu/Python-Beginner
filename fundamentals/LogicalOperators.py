54246a723f8954cdbb0bb7a86a63b3403e26a803
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-31 10:01:28 +0530

    Update LogicalOperators.py

--- fundamentals/LogicalOperators.py
+++ fundamentals/LogicalOperators.py
@@ -3,3 +3,13 @@ income = int(income)
 
 credit_score = input("Enter your credit score: ")
 credit_score = float(credit_score)
+
+if income > 50000:
+    income = True
+else:
+    income = False
+
+if credit_score >= 700:
+    credit_score = True
+else:
+    credit_score = False
