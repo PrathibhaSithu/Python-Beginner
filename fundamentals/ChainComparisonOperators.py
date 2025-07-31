f2b9e05fe18dc3a01a902390c820211ceb3e1828
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-31 10:42:08 +0530

    Create ChainComparisonOperators.py

---
+++ fundamentals/ChainComparisonOperators.py
@@ -0,0 +1,9 @@
+# age should be in between 18 and 55 to be a member in the gym
+
+age = int(input("Enter your age: "))
+
+# if age >= 18 and age <= 55:
+if 18 <= age < 65:
+    print("You are eligible to be a member of the gym.")
+else:
+    print("You are not eligible to be a member of the gym.")
