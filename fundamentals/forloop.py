0aa991b58ee803a97ef00ff8936a45a460b31904
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-31 11:03:52 +0530

    Update forloop.py

--- fundamentals/forloop.py
+++ fundamentals/forloop.py
@@ -4,8 +4,8 @@ for number in range(4):
 for pattern1 in range(4):
     print((pattern1 + 1) * "!")
 
-for pattern2 in range(4, 1):
-    print((pattern2 + 1) * "****")
+for pattern2 in range(1, 4):
+    print((pattern2 + 1) * "####")
 
-for pattern2 in range(1, 10, 2):
-    print("Line", number + 1, (pattern2 + 1) * "****")
+for pattern3 in range(1, 10, 2):
+    print("Line", pattern3 + 1, (pattern3 + 1) * "****")
