9fc1108b466ac8aa7afc8f9c49dce2af6a220562
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-07-31 10:14:50 +0530

    Update Short-Circuit.py

---
+++ fundamentals/Short-Circuit.py
@@ -0,0 +1,8 @@
+high_income = True
+good_credit = True
+Student = True
+
+if high_income and good_credit and not Student:
+    print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
