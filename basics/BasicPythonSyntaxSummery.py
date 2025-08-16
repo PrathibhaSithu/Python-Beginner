5ff8ab0adcfbbcc878b26547652c4ee5cb3856b6
PrathiSithu <sithuminiprathiba@gmail.com>
2025-08-16 11:33:30 +0530

    Update BasicPythonSyntaxSummery.py

---
+++ basics/BasicPythonSyntaxSummery.py
@@ -0,0 +1,15 @@
+# Print to Console
+print("Hello, World!")
+
+# Variable Assignment
+x = 10
+y = 5
+z = x + y
+
+# Commenting
+# This is a single-line comment
+
+"""
+This is a multi-line comment
+spanning multiple lines
+"""
