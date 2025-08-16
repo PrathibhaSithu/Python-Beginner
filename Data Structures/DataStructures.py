020cb008a69de7d2fe968467cc317dd03f67410e
PrathiSithu <sithuminiprathiba@gmail.com>
2025-08-17 00:14:54 +0530

    Update DataStructures.py

---
+++ Data Structures/DataStructures.py
@@ -0,0 +1,21 @@
+# List (Array)
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+# Accessing elements
+print(fruits[0])  # Output: apple
+print(fruits[1])  # Output: banana
+print(fruits[2])  # Output: cherry
+
+# Adding elements
+fruits.append("orange")
+print(fruits)
+
+# List Slicing
+print(fruits[1:3])
+
+# Removing elements
+fruits.remove("banana")
+print(fruits)
+
+#
