af2505d6adfc97be14185f656cea8b664f9e25d3
PrathiSithu <sithuminiprathiba@gmail.com>
2025-08-17 00:02:34 +0530

    Update FundamentalsSyntaxSummery.py

--- Summeries/FundamentalsSyntaxSummery.py
+++ Summeries/FundamentalsSyntaxSummery.py
@@ -2,3 +2,22 @@
 
 for i in range(5):
     print(i)
+
+#  While loop
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+#  Break statement
+for i in range(5):
+    if i == 3:
+        break
+    print("Break", i)
+
+#  Continue statement
+for i in range(5):
+    if i == 2:
+        continue
+    print("Continue", i)
