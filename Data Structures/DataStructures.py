f7b920ee839dcceb536291b0aa02dba55f1d8ab8
PrathiSithu <sithuminiprathiba@gmail.com>
2025-08-17 00:14:59 +0530

    Update DataStructures.py

--- Data Structures/DataStructures.py
+++ Data Structures/DataStructures.py
@@ -18,4 +18,24 @@ print(fruits[1:3])
 fruits.remove("banana")
 print(fruits)
 
-#
+# Clearing the list
+fruits.clear()
+print(fruits)  # Output: []
+
+# Tuple
+fruits = ("apple", "banana", "cherry")
+print(fruits)
+
+# Accessing elements
+print(fruits[0])  # Output: apple
+print(fruits[1])  # Output: banana
+print(fruits[2])  # Output: cherry
+
+# Adding elements to a tuple is not allowed, tuples are immutable
+
+# Set
+my_set = {1, 2, 3, 4}
+print(my_set)
+
+# Accessing elements
+# Sets do not support indexing
