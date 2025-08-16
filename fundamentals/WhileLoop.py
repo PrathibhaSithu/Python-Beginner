e33ddb019ef806043f8faef5634052c564d855f6
PrathiSithu <sithuminiprathiba@gmail.com>
2025-08-16 18:03:20 +0530

    Update WhileLoop.py

--- fundamentals/WhileLoop.py
+++ fundamentals/WhileLoop.py
@@ -3,3 +3,10 @@ command = ""
 while command.lower() != "exit":
     command = input("Enter a command (type 'exit' to quit): ")
     print("You entered:", command)
+
+
+while True:
+    command = input("> ")
+    print("You entered:", command)
+    if command.lower() == "exit":
+        break
