6ae48eb6670f8ae71e0c3127a2a935b696437ba7
PrathiSithu <sithuminiprathiba@gmail.com>
2025-08-18 00:04:17 +0530

    Update FunctionBegin.py

---
+++ Functions/FunctionBegin.py
@@ -0,0 +1,15 @@
+# 2 Types of functions
+
+# - Performe a task
+# greet
+# print
+# - Return a value
+# round
+# get_greeting
+
+def greet(name):
+    print(f"Hi {name}")
+
+
+def get_greeting(name):
+    return f"Hi {name}"
