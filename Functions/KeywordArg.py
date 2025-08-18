f886d5e2fdc0188255e330180379469e2bfcdcd5
PrathiSithu <sithuminiprathiba@gmail.com>
2025-08-18 12:51:50 +0530

    Update KeywordArg.py

--- Functions/KeywordArg.py
+++ Functions/KeywordArg.py
@@ -8,4 +8,8 @@ print(result)
 
 #  or we can use this
 
-print(increment(9, 1))  # This will also print the result directly
+print(increment(9, by=1))  # This will also print the result directly
+
+# by=1 is a keyword argument
+
+# keyword arguments allow you to specify arguments by their parameter name
